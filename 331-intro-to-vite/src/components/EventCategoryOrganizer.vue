34c0554d8b5d8f61390dfb55766a12dded29bc29
Nawapon <nawapon_s@cmu.ac.th>
2025-08-14 23:20:52 +0700

    Add EventCategoryOrganizer component

---
+++ 331-intro-to-vite/src/components/EventCategoryOrganizer.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { Event } from '@/types'
+
+defineProps<{
+  event: Event
+}>()
+</script>
+
+<template>
+  <div class="flex flex-col items-end text-base mb-2.5">
+    <span class="text-right text-base">{{ event.category }}</span>
+    <span class="text-right text-base">{{ event.organizer }}</span>
+  </div>
+</template>
+
+<style scoped>
+/* All CSS has been converted to Tailwind classes */
+</style>
